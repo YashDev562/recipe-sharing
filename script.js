2e0f5666c775aa156052437094ca7ed2cc678b0e
swayam-gaded <230393153+swayam-gaded@users.noreply.github.com>
2025-12-14 11:45:51 +0530

    Finished the add feature created the box model for each recipe and put the styling typed the js code for when the add button is clicked

---
+++ script.js
@@ -0,0 +1,47 @@
+const addBtn = document.getElementById("add-button");
+const model = document.getElementById("model-box");
+const submitBtn = document.getElementById("addNewRecipe");
+const cancelBtn = document.getElementById("closeModel");
+const nameInput = document.getElementById("nameInput");
+const descInput = document.getElementById("descInput");
+
+addBtn.addEventListener('click', () => {
+    model.style.display = 'flex';
+});
+
+cancelBtn.addEventListener('click', () => {
+    model.style.display = 'none';
+    nameInput.value = "";
+    descInput.value = "";
+});
+
+submitBtn.addEventListener('click', () => {
+    const newRecipeName = nameInput.value.trim();
+    const newRecipeDesc = descInput.value.trim();
+    if(newRecipeDesc == "" || newRecipeName == "") {
+        alert("Fill both the fields");
+        return;
+    }
+
+    const newBox = document.createElement('div');
+    newBox.classList.add('recipe-box');
+    const image = document.createElement('div');
+    image.classList.add('image');
+    const content = document.createElement('div');
+    content.classList.add('recipe-content');
+    const name = document.createElement('div');
+    name.textContent = newRecipeName;
+    const desc = document.createElement('div');
+    desc.textContent = newRecipeDesc;
+
+    content.appendChild(name);
+    content.appendChild(desc);
+    newBox.appendChild(image);
+    newBox.appendChild(content);
+    document.querySelector('.main').appendChild(newBox);
+
+    model.style.display = 'none';
+    nameInput.value = "";
+    descInput.value = "";
+})
+
