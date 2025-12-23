0e2f08daf5646e530b84b61df8b81edbe13a1510
YashDev562 <yashgd562@gmail.com>
2025-12-23 11:45:34 +0530

    Added JS logic for checking and validating login credentials

---
+++ login.js
@@ -0,0 +1,29 @@
+document.getElementById("login_info").addEventListener("submit", async e => {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const user = {
+    username : username,
+    password : password
+  }
+
+  try {
+    const response = await fetch("verifyCredentials.php", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user)
+    });
+
+    const data = await response.json();
+
+    if (data.isValidCredential) {
+      window.location.href = "index.html";
+    } else {
+      alert("Invalid Login Credentials, please try again");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Something went wrong. Please try again later.");
+  }
+});
